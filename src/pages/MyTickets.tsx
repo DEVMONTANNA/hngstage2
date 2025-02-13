0226b4acdc2ec62a193e73aa2134be21311fc855
DEVMONTANNA <chiedoziegoodluck351@gmail.com>
2025-02-13 19:01:50 +0100

    chore: Fixed routing bug

--- src/pages/MyTickets.tsx
+++ src/pages/MyTickets.tsx
@@ -24,7 +24,7 @@ const MyTicket: React.FC = () => {
   }, []);
   return (
     <div>
-      {user?.name ? (
+      {!user?.name ? (
         <div className="flex justify-center items-center">
           <div>
             <p className="mt-2 text-center text-lg text-gray-600 dark:text-gray-400">
