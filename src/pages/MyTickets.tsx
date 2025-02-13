ab29062027074143db8fad088a9b2620ba8309e9
DEVMONTANNA <chiedoziegoodluck351@gmail.com>
2025-02-13 18:59:01 +0100

    chore: Fixed routing bug

--- src/pages/MyTickets.tsx
+++ src/pages/MyTickets.tsx
@@ -1,12 +1,50 @@
-import React from 'react'
-import MyTicketComponent from '../components/MyTicketsComponent'
+import React, { useEffect, useState } from "react";
+import MyTicketComponent from "../components/MyTicketsComponent";
+import { Link } from "react-router-dom";
+import ButtonBg from "../components/buttons/ButtonBg";
 
-const MyTicket: React.FC = () => {
-  return (
-    <div>
-      <MyTicketComponent/>
-    </div>
-  )
+interface User {
+  name: string;
+  email: string;
+  selectedFile: string;
+  specialRequest: string;
 }
 
-export default MyTicket
+const MyTicket: React.FC = () => {
+  const [user, setUser] = useState<User>({
+    name: "",
+    email: "",
+    selectedFile: "",
+    specialRequest: "",
+  });
+  useEffect(() => {
+    const getUserData = localStorage.getItem("user") || "{}";
+    const parsedUserData = JSON.parse(getUserData);
+    setUser(parsedUserData);
+  }, []);
+  return (
+    <div>
+      {user?.name ? (
+        <div className="flex justify-center items-center">
+          <div>
+            <p className="mt-2 text-center text-lg text-gray-600 dark:text-gray-400">
+              The page you're looking for doesn't exist or has been moved.
+            </p>
+            <div className="flex justify-center items-center">
+              <Link
+                to="/"
+                className="mt-6 rounded-lg  px-6 py-3 text-lg font-medium text-white transition"
+              >
+                <ButtonBg type="button" text="Go Back Home" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <MyTicketComponent />
+      )}
+    </div>
+  );
+};
+
+export default MyTicket;
