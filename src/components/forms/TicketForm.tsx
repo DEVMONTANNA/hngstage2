2b5cdca62ddb041e12bbe184a576ed48061b8410
DEVMONTANNA <chiedoziegoodluck351@gmail.com>
2025-02-14 16:20:01 +0100

    Chore: Updated responsiveness

--- src/components/forms/TicketForm.tsx
+++ src/components/forms/TicketForm.tsx
@@ -40,7 +40,7 @@ const TicketForm: React.FC<TicketFormProps> = ({
             {tickets.map((ticket) => (
               <div
                 key={ticket}
-                className={`w-full md:w-[158px]  h-[110px] p-3 pb-4 cursor-pointer rounded-xl border-2 flex flex-col justify-start items-start gap-3 overflow-hidden ${
+                className={`w-full md:w-[158px]  h-[110px] p-3  cursor-pointer rounded-xl border-2 flex flex-col justify-start items-start gap-3 overflow-hidden ${
                   selectedTicket === ticket
                     ? "border-[#24A0B5] bg-[#197686]"
                     : "border-[#197686]"
@@ -53,7 +53,7 @@ const TicketForm: React.FC<TicketFormProps> = ({
                 <span className="text-neutral-50 text-sm md:text-base uppercase">
                   {ticket} Access
                 </span>
-                <p>20/52</p>
+                <p className="pb-2">20/52</p>
               </div>
             ))}
           </div>
